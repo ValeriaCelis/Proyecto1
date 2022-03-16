c494f4efb82643e6b5cd4b66a2f65661c9826a34
ValeriaCelis <avca0414@gmail.com>
2022-03-15 22:29:25 -0700

    Archivo index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diseñador Freelancer</title>
+</head>
+<body>
+    <header>
+        <h1>Valeria Celis Freelancer</h1>
+    </header>
+    
+    <section>
+        <h2>Diseño y Desarrollo Web Freelancer</h2>
+        <p>Mazatlan, Sinaloa</p>
+    </section>
+
+<main>
+    <h2>Mis Servicios</h2>
+
+    <section>
+        <h3>Diseño Web</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda laudantium molestias
+           labore! Dicta.</p>
+    </section>
+
+    <sectio>
+        <h3>Aplicaciones Web</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda laudantium molestias
+            labore! Dicta.</p>
+    </sectio>
+
+    <section>
+        <h3>E-Commerce</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda laudantium molestias
+            labore! Dicta.</p>
+    </section>
+
+    <h2>Contacto</h2>
+    <p>Todos los derechos reservador. Valeria Celis Freelancer</p>
+</main>
+    
+</body>
+</html>
